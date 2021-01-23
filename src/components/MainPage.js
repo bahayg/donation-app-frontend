7f87d4bbb0f3c20e8508b51826e12588d73399ef
bahayg <bahayg@gmail.com>
2021-01-23 10:54:41 -0800

    Add semi colons

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,60 +1,83 @@
-import React, { Component} from 'react'
-import {Header, Segment, Dropdown, Grid} from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Header, Segment, Dropdown, Grid } from "semantic-ui-react";
 
 class MainPage extends Component {
+  componentDidMount() {
+    this.uniqCities();
+    this.props.getCharities();
+  }
 
-    componentDidMount() {
-        this.uniqCities()
-        this.props.getCharities()
+  uniqCities = () => {
+    if (!this.props.allCharities.message) {
+      const uniq = this.props.allCharities
+        .map((charity) => charity.city)
+        .filter((v, i, a) => a.indexOf(v) === i)
+        .sort()
+        .map((city) => city);
+      uniq.unshift("All");
+      return uniq.map((city, index) => (
+        <div className="item" key={index}>
+          <li>{city}</li>
+        </div>
+      ));
+    } else {
+      return null;
     }
-    
-    uniqCities = () => {
-        if (!this.props.allCharities.message) {
-            const uniq = this.props.allCharities.map(charity => charity.city).filter((v, i, a) => a.indexOf(v) === i).sort().map(city => city);
-            uniq.unshift("All") 
-            return uniq.map((city, index) => <div className="item" key={index}><li>{city}</li></div>)
-        } else {
-            return null
-        }
-      }
+  };
 
-      handleCitySelect = (e) => {
-        const city = e.target.innerText
-        this.props.changeCity(city)
-        this.props.history.push(`/charities/${city}`)
-      }
+  handleCitySelect = (e) => {
+    const city = e.target.innerText;
+    this.props.changeCity(city);
+    this.props.history.push(`/charities/${city}`);
+  };
 
-    render () {
-        return (
-            <>
-            <div className='home-segment'>
-            <Grid columns={3}  style={{ paddingTop: '2.5rem', paddingBottom: '1.5rem' }}textAlign='center'>
-                <Grid.Column >
-                <Segment circular style = {{ width: 350, height: 350, opacity: 0.85 }}>
-                    <Header as='h1' textAlign='center'>
-                        Welcome to Reunited Hands
-                        <br></br>
-                    </Header>
-                    <p>
-                        My app will connect individuals with surplus items to local organizations and charities who are in need. 
-                    </p>
-                </Segment>
-                </Grid.Column>
-            </Grid>
+  render() {
+    return (
+      <>
+        <div className="home-segment">
+          <Grid
+            columns={3}
+            style={{ paddingTop: "2.5rem", paddingBottom: "1.5rem" }}
+            textAlign="center"
+          >
+            <Grid.Column>
+              <Segment
+                circular
+                style={{ width: 350, height: 350, opacity: 0.85 }}
+              >
+                <Header as="h1" textAlign="center">
+                  Welcome to Reunited Hands
+                  <br></br>
+                </Header>
+                <p>
+                  My app will connect individuals with surplus items to local
+                  organizations and charities who are in need.
+                </p>
+              </Segment>
+            </Grid.Column>
+          </Grid>
 
-            <Grid columns={3}  textAlign='center' fontSize="large">
-                <Grid.Column>
-                <Dropdown className='city-dropdown' item direction="left" text= 'Select a City to Explore Local Organizations'>
-                    <Dropdown.Menu>
-                        <Dropdown.Item  onClick={(e) => this.handleCitySelect(e)}>{this.uniqCities()}</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-                </Grid.Column>
-            </Grid>
-            </div>
-            </>
-        )
-    }
-} 
+          <Grid columns={3} textAlign="center" fontSize="large">
+            <Grid.Column>
+              <Dropdown
+                className="city-dropdown"
+                floating
+                item
+                direction="left"
+                text="Select a City to Explore Local Organizations"
+              >
+                <Dropdown.Menu>
+                  <Dropdown.Item onClick={(e) => this.handleCitySelect(e)}>
+                    {this.uniqCities()}
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Grid.Column>
+          </Grid>
+        </div>
+      </>
+    );
+  }
+}
 
-export default MainPage
+export default MainPage;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,35 +1,61 @@
-import React, { Component } from 'react';
-import { Menu, Image } from 'semantic-ui-react'
-import { NavLink} from  'react-router-dom';
+import React, { Component } from "react";
+import { Menu, Image } from "semantic-ui-react";
+import { NavLink } from "react-router-dom";
 
-class NavBar extends Component{
+class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <Image size="medium" src="/LogoV2.png" centered />
 
-  render() {    
-    return(
-      <div> 
-        <Image size="medium" src='/LogoV2.png' centered/>   
+        <Menu
+          pointing
+          secondary
+          style={{ paddingTop: "0.1em", paddingBottom: "0.1em" }}
+        >
+          <Menu.Item
+            floated="left"
+            name="home"
+            as={NavLink}
+            to="/home"
+          ></Menu.Item>
 
-        <Menu pointing secondary style={{ paddingTop: '0.1em', paddingBottom: '0.1em' }} >
+          {this.props.user.id && this.props.user.is_admin ? (
+            <Menu.Item
+              floated="left"
+              onClick={this.props.onGetAdminsCharities}
+              as={NavLink}
+              to={`/users/${this.props.user.username}/charities`}
+            >{`${this.props.user.username}'s Profile`}</Menu.Item>
+          ) : this.props.user.id && !this.props.user.is_admin ? (
+            <Menu.Item
+              floated="left"
+              onClick={this.props.onGetUserRequests}
+              as={NavLink}
+              to={`/users/${this.props.user.username}/requests`}
+            >{`${this.props.user.username}'s Profile`}</Menu.Item>
+          ) : null}
 
-            <Menu.Item floated='left' name='home' as={NavLink} to='/home'></Menu.Item>
-            
-            {this.props.user.id && this.props.user.is_admin ? (<Menu.Item floated='left'  onClick={this.props.onGetAdminsCharities} as={NavLink} to={`/users/${this.props.user.username}/charities`}>{`${this.props.user.username}'s Profile`}</Menu.Item>
-            ) 
-            :
-            (this.props.user.id && !this.props.user.is_admin ? (<Menu.Item floated='left'  onClick={this.props.onGetUserRequests} as={NavLink} to={`/users/${this.props.user.username}/requests`}>{`${this.props.user.username}'s Profile`}</Menu.Item>)
-            :
-            null)
-          }
-            
-            {this.props.user.id ?  (<Menu.Item floated='left' onClick={this.props.onLogout} as={NavLink} to="/login">Log Out</Menu.Item>
-            ) 
-            : 
-            (
-              <Menu.Item floated='left' as={NavLink} to='/login' name='Log In'></Menu.Item>)
-            }
+          {this.props.user.id ? (
+            <Menu.Item
+              floated="left"
+              onClick={this.props.onLogout}
+              as={NavLink}
+              to="/login"
+            >
+              Log Out
+            </Menu.Item>
+          ) : (
+            <Menu.Item
+              floated="left"
+              as={NavLink}
+              to="/login"
+              name="Log In"
+            ></Menu.Item>
+          )}
         </Menu>
-      </div> 
-    )
+      </div>
+    );
   }
 }
 
