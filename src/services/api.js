490c7656e9c14e41d87871950ba3bf5dac58ce43
bahayg <bahayg@gmail.com>
2021-01-24 08:16:38 -0800

    Add env variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,8 @@
-const API_ROOT = `https://fathomless-journey-52654.herokuapp.com/`;
+//const API_ROOT = `https://fathomless-journey-52654.herokuapp.com/`;
+
+require("dotenv").config();
+
+const API_ROOT = process.env.REACT_APP_BASE_URL || "http://localhost.3000";
 
 const token = () => localStorage.getItem("token");
 
