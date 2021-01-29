dd5d1446241b789e024257b18271403687cedabe
bahayg <bahayg@gmail.com>
2021-01-29 09:30:12 -0800

    Make image and font size responsive

--- src/components/AdminsCharityCard.js
+++ src/components/AdminsCharityCard.js
@@ -1,29 +1,34 @@
 import React, { Component } from "react";
-import { Card, Image } from 'semantic-ui-react'
-import { NavLink } from 'react-router-dom'
+import { Card, Image } from "semantic-ui-react";
+import { NavLink } from "react-router-dom";
 
- class AdminsCharityCard extends Component {
+class AdminsCharityCard extends Component {
+  handleClick = () => {
+    this.props.onShowCharityDetails(this.props.charity);
+    this.props.onGetCharityRequests(this.props.charity.id);
+  };
 
-    handleClick = () => {
-        this.props.onShowCharityDetails(this.props.charity)
-        this.props.onGetCharityRequests(this.props.charity.id)
-    }
+  render() {
+    return (
+      <Card
+        onClick={() => this.handleClick()}
+        as={NavLink}
+        to={`charities/${this.props.charity.id}`}
+      >
+        <Card.Content style={{ backgroundColor: "#F7B267" }}>
+          <Card.Header style={{ fontSize: "100%" }}>
+            {this.props.charity.name}
+          </Card.Header>
+        </Card.Content>
 
-     render() {
+        <Card.Content
+          style={{ borderColor: "transparent", backgroundColor: "#F7B267" }}
+        >
+          <Image src={this.props.charity.image} size="medium" rounded />
+        </Card.Content>
+      </Card>
+    );
+  }
+}
 
-         return(
-
-            <Card onClick={() => this.handleClick() } as={NavLink} to={`charities/${this.props.charity.id}`} >
-                <Card.Content style={{backgroundColor: '#F7B267'}}>
-                    <Card.Header>{this.props.charity.name}</Card.Header>
-                </Card.Content>
-
-                <Card.Content style={{borderColor: 'transparent', backgroundColor: '#F7B267'}}>
-                    <Image src={this.props.charity.image} size="medium" rounded />
-                </Card.Content>
-            </Card>
-         )
-     }
- }
-
- export default AdminsCharityCard
+export default AdminsCharityCard;
